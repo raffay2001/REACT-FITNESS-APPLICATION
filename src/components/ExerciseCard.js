44491812ab66b65d72b38eb7cdd47bf0cd3b7b91
raffay2001 <raffay.ahmed009@gmail.com>
2022-09-27 20:07:39 +0500

    Worked On the Exercise Card  Component

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
+
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link to={`/exercise/${exercise.id}`} className="exercise-card">
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#FFFFFF",
+            background: "#FFA9A9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#FFFFFF",
+            background: "#FCC757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        ml="21px"
+        color="#000000"
+        fontWeight="bold"
+        marginTop="11px"
+        pb="10px"
+        textTransform="capitalize"
+        fontSize="22px"
+      >
+        {exercise.name}
+      </Typography>
+    </Link>
+  );
+};
+
+export default ExerciseCard;
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
@@ -16,7 +17,10 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         justifyContent="center"
       >
         {exercises.map((exercise, index) => (
-          <p>{exercise.name}</p>
+          <ExerciseCard
+            key={exercise.name.toString() + index.toString()}
+            exercise={exercise}
+          />
         ))}
       </Stack>
     </Box>
