4ca0b548018f3e9f5811f0eb3f2926e3fb603bee
raffay2001 <raffay.ahmed009@gmail.com>
2022-09-27 22:05:24 +0500

    Worked on Exercise Detail Page

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { fetchData, exerciseOptions } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
   return <div>ExerciseDetail</div>;
